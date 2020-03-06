acf4ed48ba0c1ae3d12680d65eb1c87f72ea6d74
Jeffrey Lan <jtlan.sf@gmail.com>
2020-03-06 16:43:44 -0500

    added recipes

--- biskwee-app/db/seeds.rb
+++ biskwee-app/db/seeds.rb
@@ -61,7 +61,7 @@ Ingredient.create!(name: "cake flour", category_id: 3, unit_id: 1)
 Ingredient.create!(name: "almond paste", category_id: 4, unit_id: 1)
 Ingredient.create!(name: "praline paste", category_id: 4, unit_id: 1)
 Ingredient.create!(name: "peanut butter", category_id: 4, unit_id: 1, notes:"smooth")
-Ingredient.create!(name: "almond flour", category_id: 4, unit_id: 1)
+Ingredient.create!(name: "sliced almonds", category_id: 4, unit_id: 1)
 
 Ingredient.create!(name: "lemon juice", category_id: 5 , unit_id: 1)
 Ingredient.create!(name: "lemons, zest only", category_id: 5 , unit_id: 15)
@@ -74,7 +74,7 @@ Ingredient.create!(name: "dark chocolate couverture", category_id: 6, unit_id: 1
 Ingredient.create!(name: "cocoa powder", category_id: 6, unit_id: 1)
 
 Ingredient.create!(name: "vanilla bean", category_id: 7, unit_id: 15, notes: "halved and scraped")
-Ingredient.create!(name: "Earl Grey tea leaves", category_id: 7, unit_id: 1)
+Ingredient.create!(name: "matcha", category_id: 7, unit_id: 1)
 Ingredient.create!(name: "Vietnamese cinnamon powder", category_id: 7, unit_id: 1)
 
 Ingredient.create!(name: "water", category_id:8, unit_id:1)
@@ -85,6 +85,7 @@ Ingredient.create!(name: "sorbet stabilizer", category_id: 8, unit_id: 1, notes:
 Ingredient.create!(name: "baking soda", category_id: 8, unit_id: 1)
 Ingredient.create!(name: "baking powder", category_id: 8, unit_id: 1)
 Ingredient.create!(name:"salt",category_id:8, unit_id:1)
+Ingredient.create!(name:"pastry cream", category_id:8,unit_id:1)
 
 # Sample [Recipes]
 Recipe.create!(name: "Pâte à Choux ",yield_qty: 800,unit_id: 1,ingredients: [
@@ -105,6 +106,27 @@ cool a bit for one minute.",
 "At low speed, beat in the eggs, one at a time, beating until incorporated between additions.
 The dough should look smooth and glossy, stiff but not dry.",
 "Transfer dough to a pastry bag with a plain tip and pipe out as desired."
-],image_url: "https://assets.marthastewart.com/styles/wmax-1500/d22/mb_1008_pate_a_choux_2/mb_1008_pate_a_choux_2_horiz.jpg", notes: "yield is approximate", parent_recipes: [2])
-# Recipe.create!(name: "",yield_qty: ,unit_id: ,ingredients: [],method: [],image_url: "", notes: "", parent_recipes: [])
-# Recipe.create!(name: "",yield_qty: ,unit_id: ,ingredients: [],method: [],image_url: "", notes: "", parent_recipes: [])
+],image_url: "https://assets.marthastewart.com/styles/wmax-1500/d22/mb_1008_pate_a_choux_2/mb_1008_pate_a_choux_2_horiz.jpg", notes: "yield is approximate", parent_recipes: [3])
+Recipe.create!(name: "Green Tea Biscuit",yield_qty: 1,unit_id: 15,ingredients: [
+	{qty: 60,unit_id: 1,ingredient_id: 15},
+	{qty: 15,unit_id: 1,ingredient_id: 29},
+	{qty: 150,unit_id: 1,ingredient_id: 24, notes: "melted"},
+	{qty: 120,unit_id: 1,ingredient_id: 11, notes: "softened"},
+	{qty: 75,unit_id: 1,ingredient_id: 8},
+	{qty: 25,unit_id: 1,ingredient_id: 3},
+	{qty: 1,unit_id: 15,ingredient_id: 21, notes: "finely grated"},
+	{qty: 125,unit_id: 1,ingredient_id: 7},
+	{qty: 65,unit_id: 1,ingredient_id: 1},
+],
+method: [
+	"Sift cake flour with matcha and reserve. Thoroughly incorporate butter into melted chocolate. Stir in egg
+yolks, trimoline, and lemon zest.",
+"Meanwhile, begin whipping egg whites, gradually adding sugar, to soft peaks.",
+"Fold in sifted cake flour, followed by white chocolate base. Transfer into a prepared half sheet pan.",
+"Bake at 325ºF for 10 to 13 minutes. Allow to cool, store wrapped under refrigeration."
+],image_url: "https://3.bp.blogspot.com/-HUG2O_5yoW4/TdjlJ8Oju5I/AAAAAAAACOI/7YnrLzIWoJ4/s1600/biscocho.jpg", notes: "", parent_recipes: [])
+Recipe.create!(name: "Praline Cream",yield_qty: 450,unit_id: 1,ingredients: [
+	{qty: 120, unit_id: 1,ingredient_id: 11},
+	{qty: 90, unit_id: 1,ingredient_id: 17},
+	{qty: 240, unit_id: 1,ingredient_id: 39}],method: ["Combine butter and praline paste in a mixer bowl and whip until thoroughly combined. Slowly add
+pastry cream.","Chill for use later, or store at room temperature until assembly of dessert."],image_url: "", notes: "", parent_recipes: [3])
