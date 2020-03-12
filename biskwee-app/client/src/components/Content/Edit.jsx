21faeae60281994b0180a1f4b5bb4c3b01b0f703
Jeffrey Lan <jtlan.sf@gmail.com>
2020-03-12 09:35:11 -0400

    ingredient input renders

--- biskwee-app/client/src/components/Content/Edit.jsx
+++ biskwee-app/client/src/components/Content/Edit.jsx
@@ -11,6 +11,7 @@ class Edit extends Component {
       _isMounted: false,
       recipeIsLoaded: false,
       all_units: this.props.all_units,
+      all_ingredients: this.props.all_ingredients,
       newRecipe: {
         name: "",
         ingredients: [],
@@ -20,16 +21,20 @@ class Edit extends Component {
   }
 
   componentDidMount() {
-    // console.log("edit state", this.state);
+    console.log("edit state", this.state);
     console.log("edit props", this.props);
     this.setState({ _isMounted: true });
   }
+
+  handleChange = e => {
+    //set to state
+  };
 
   makeInput = (key, val) => {
     return !this.props.current_recipe ? (
       <input placeholder={`${key}`} required />
     ) : (
-      <input placeholder={`${key}`} value={val} required />
+      <input placeholder={`${key}`} defaultValue={val} required />
     );
   };
 
@@ -39,48 +44,70 @@ class Edit extends Component {
       unit_id,
       ingredient_id
     } = this.props.current_recipe.ingredients;
-    return lines.map(li => (
+    return lines.map((li, i) => (
       <div>
-        <input id="qty" type="number" value={li.line.qty} />
+        <input id="qty" type="number" defaultValue={li.line.qty} key={i} />
+
+        <select id="unit">
+          <option defaultValue={li.line.unit_id}>
+            {this.state.all_units[li.line.unit_id - 1].name}
+          </option>
+          {this.state.all_units.map((unit, i) => (
+            <option value={unit.id} key={i}>
+              {unit.name}
+            </option>
+          ))}
+        </select>
+
+        <select id="ingredient">
+          <option defaultValue={li.line.ingredient_id}>
+            {this.state.all_ingredients[li.line.ingredient_id - 1].name}
+          </option>
+          {this.state.all_ingredients.map((ingred, i) => (
+            <option value={ingred.id} key={i}>
+              {ingred.name}
+            </option>
+          ))}
+        </select>
       </div>
     ));
   };
 
-  makeIngred = (qty, unit_id, ingredient_id) => {
-    return (
-      <div>
-        <label for="qty">amount:</label>
-        <input id="qty" type="number" value={qty} />
+  // makeIngred = (qty, unit_id, ingredient_id) => {
+  //   return (
+  //     <div>
+  //       <label for="qty">amount:</label>
+  //       <input id="qty" type="number" value={qty} />
 
-        <label for="unit">unit</label>
-        <select id="unit" className="unit_id">
-          {this.props.all_units.length > 1 &&
-            console.log("UNITS", this.state.all_units[unit_id - 1])}
-          <option selected="selected">
-            {this.props.all_units.length > 1 &&
-              this.props.all_units[unit_id - 1].name}
-          </option>
-          {this.props.all_units.length > 1 &&
-            this.props.all_units.map(unit => (
-              <option value={`${unit.name}`}>{unit.abbrev}</option>
-            ))}
-        </select>
+  //       <label for="unit">unit</label>
+  //       <select id="unit" className="unit_id">
+  //         {this.props.all_units.length > 1 &&
+  //           console.log("UNITS", this.state.all_units[unit_id - 1])}
+  //         <option selected="selected">
+  //           {this.props.all_units.length > 1 &&
+  //             this.props.all_units[unit_id - 1].name}
+  //         </option>
+  //         {this.props.all_units.length > 1 &&
+  //           this.props.all_units.map(unit => (
+  //             <option value={`${unit.name}`}>{unit.abbrev}</option>
+  //           ))}
+  //       </select>
 
-        <label for="ingredient">ingredient</label>
-        <select id="ingredient" className="ingredient_id">
-          (!this.props.all_ingredients)?<p>...</p>:
-          <option selected="selected">
-            {this.props.all_ingredients.length > 1 &&
-              this.props.all_ingredients[ingredient_id - 1].name}
-          </option>
-          {this.props.all_ingredients.length > 1 &&
-            this.props.all_ingredients.map(ingred => (
-              <option value={`${ingred.name}`}>{ingred.name}</option>
-            ))}
-        </select>
-      </div>
-    );
-  };
+  //       <label for="ingredient">ingredient</label>
+  //       <select id="ingredient" className="ingredient_id">
+  //         (!this.props.all_ingredients)?<p>...</p>:
+  //         <option selected="selected">
+  //           {this.props.all_ingredients.length > 1 &&
+  //             this.props.all_ingredients[ingredient_id - 1].name}
+  //         </option>
+  //         {this.props.all_ingredients.length > 1 &&
+  //           this.props.all_ingredients.map(ingred => (
+  //             <option value={`${ingred.name}`}>{ingred.name}</option>
+  //           ))}
+  //       </select>
+  //     </div>
+  //   );
+  // };
 
   /*---------- RENDER ---------- */
   render() {
@@ -93,7 +120,10 @@ class Edit extends Component {
     return (
       <form>
         {this.makeInput("Recipe name", current_recipe.name)}
-        {this.editIngred(this.props.current_recipe.ingredients)}
+        {this.editIngred(
+          this.props.current_recipe.ingredients.length > 1 &&
+            this.props.current_recipe.ingredients
+        )}
       </form>
     );
   }
