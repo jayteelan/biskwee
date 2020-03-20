e95096bc3e88de949b2df98eb834a0171ff3bcef
Jeffrey Lan <jtlan.sf@gmail.com>
2020-03-20 00:15:34 -0400

    FULL FRONTEND CRUD OMG

--- biskwee-app/app/models/recipe.rb
+++ biskwee-app/app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ApplicationRecord
 	belongs_to :unit
-	has_many :ingred_lines
+	has_many :ingred_lines, dependent: :delete_all
 	has_many :ingredients, :through=>:ingred_lines
 end
--- biskwee-app/client/src/components/Main/Detail/Detail.jsx
+++ biskwee-app/client/src/components/Main/Detail/Detail.jsx
@@ -1,30 +1,37 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { getData, deleteRecipe } from "../../../api-helper";
 
 import IngredList from "./IngredList";
 import MethodList from "./MethodList";
 
-const Detail = props => {
-  // class Detail extends Component {
-  //   constructor(props) {
-  //     super(props);
+// const Detail = props => {
+class Detail extends Component {
+  constructor(props) {
+    super(props);
 
-  //     this.state = {};
-  //   }
+    this.state = {};
+  }
 
-  //   componentDidMount() {
-  //     console.log("DETAIL", this.props);
-  //   }
+  componentDidMount = async () => {
+    this.setState(await getData("recipes", this.props.match));
+  };
 
-  // render() {
-  return (
-    <div>
-      <Link to={`/recipes/${props.match}/edit`}>Edit this recipe</Link>
-      <IngredList {...props} />
-      <MethodList match={props.match} />
-    </div>
-  );
-  // }
-};
+  handleDelete = async () => {
+    await deleteRecipe(this.props.match);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.name}</h1>
+        <Link to={`/recipes/${this.props.match}/edit`}>Edit this recipe</Link>
+        <button onClick={this.handleDelete}>Delete this recipe</button>
+        <IngredList {...this.props} />
+        <MethodList match={this.props.match} />
+      </div>
+    );
+  }
+}
 
 export default Detail;
